6014fe4052a729167d6041dbdb8d052abcd25ada
Hoang Nguyen <hoangnguyen@Hoangs-MacBook-Pro.local>
2023-10-18 01:29:29 +0200

    firebase.js and callscreen

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import 'firebase/compat/database';
-
+import 'firebase/compat/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDz4QogR6MQBhXrz8Kheyqhg-Ka1P80KLs",
@@ -11,7 +11,8 @@ const firebaseConfig = {
   messagingSenderId: "1065876130644",
   appId: "1:1065876130644:web:d16143bbd785729cc156c1",
   measurementId: "G-W2WZ7SQJTC",
-  databaseURL: "https://qubit-65f65-default-rtdb.firebaseio.com/"
+  databaseURL: "https://qubit-65f65-default-rtdb.firebaseio.com/",
+  storageBucket: 'gs://qubit-65f65.appspot.com'
 };
 
 export function writeUserData(userId, email) {
@@ -26,7 +27,7 @@ export function writeUserData(userId, email) {
   });
 }
 
-export function writeUserContacts(userId, contactName, phoneNumber) {
+export function writeUserContacts(userId, contactFirstName, contactLastName, phoneNumber) {
   const database = firebase.database();
   const userContactsRef = database.ref(`contacts/${userId}`);
   
@@ -35,14 +36,27 @@ export function writeUserContacts(userId, contactName, phoneNumber) {
 
   // Data to store for the contact
   const contactData = {
-    contactName: contactName,
+    contactFirstName: contactFirstName,
+    contactLastName: contactLastName,
     phoneNumber: phoneNumber,
   };
-
+  console.log("Contact added")
   // Set the contact data under the unique key
-  userContactsRef.child(newContactKey).set(contactData);
+  const newContactRef = userContactsRef.push(contactData);
 }
 
+export function handleImageUpload(event) {
+  const file = event.target.files[0];
+  if (!file) return;
+
+  const storageRef = firebase.storage().ref();
+  const fileRef = storageRef.child("images/" + file.name);
+
+  return fileRef.put(file).then((snapshot) => {
+    console.log("Uploaded an image!");
+    return fileRef.getDownloadURL();
+  });
+}
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -50,4 +64,5 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = firebase.auth();
 const database = firebase.database();
-export { auth, database };
+const storage = firebase.storage();
+export { auth, database, storage };
--- screens/CallsScreen.js
+++ screens/CallsScreen.js
@@ -1,22 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import {writeUserContacts} from '../firebase';
-
+import firebase from 'firebase/compat';
 const CallScreen = ({ uid }) => {
-  const [contactName, setContactName] = useState('');
+  const [contactFirstName, setContactFirstName] = useState('');
+  const [contactLastName, setContactLastName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [contacts, setContacts] = useState([]);
 
   const handleAddContact = () => {
-    writeUserContacts(uid, contactName, phoneNumber);
+    writeUserContacts(uid, contactFirstName, contactLastName, phoneNumber);
   };
+
+  useEffect(() => {
+    const userId = uid;
+    const userContactsRef = firebase.database().ref(`contacts/${userId}`);
+
+    // This listener will get triggered every time the data changes
+    const handleDataChange = snapshot => {
+        if (snapshot.val()) {
+            // Convert Firebase object to array of contact objects
+            const firebaseContactsArray = Object.values(snapshot.val());
+            setContacts(firebaseContactsArray);
+        }
+    };
+
+    userContactsRef.on('value', handleDataChange);
+
+    // Clean up listener on component unmount
+    return () => {
+        userContactsRef.off('value', handleDataChange);
+    };
+}, []);
 
   return (
     <View style={styles.container}>
+      {contacts.map((contact, index) => (
+            <View key={index} style={{ marginBottom: 20 }}>
+                <Text>First name: {contact.contactFirstName} Last name: {contact.contactLastName}</Text>
+                <Text>Number: {contact.phoneNumber}</Text>
+            </View>
+        ))}
       <Text style={styles.title}>Add Contact</Text>
       <TextInput
-        placeholder="Contact Name"
-        value={contactName}
-        onChangeText={setContactName}
+        placeholder="Contact First Name"
+        value={contactFirstName}
+        onChangeText={setContactFirstName}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Contact Last Name"
+        value={contactLastName}
+        onChangeText={setContactLastName}
         style={styles.input}
       />
       <TextInput
